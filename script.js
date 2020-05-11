9d03c8cbfa1ec6b23ee5dea0af0f877ee46a9e8c
Nissa <nmnissa@gmail.com>
2020-05-11 14:54:52 -0700

    Made winning possible

--- script.js
+++ script.js
@@ -7,39 +7,67 @@ function computerPlay() {
 function playRound() { 
     const playerSelection = this.id; 
     const computerSelection = computerPlay(); 
-    const message = document.querySelector("#message"); 
+    let message = document.querySelector("#message"); 
+    let playerScore = document.querySelector("#player-score");
+    let computerScore = document.querySelector("#computer-score"); 
+    const playerScoreVal = parseInt(playerScore.textContent, 10);
+    const computerScoreVal = parseInt(computerScore.textContent, 10); 
  
+    console.log("clicked"); 
+
     if (playerSelection == computerSelection) { 
         message.textContent = "Tie!"; 
     } else if (playerSelection == "rock") { 
         if ( computerSelection == "scissors") { 
             message.textContent = "You Win! Rock beats scissors.";
-            return 1;
+            playerScore.textContent = playerScoreVal + 1;
         } else { 
             message.textContent = "You Lose! Paper beats rock.";
-            return 0;
+            computerScore.textContent = computerScoreVal + 1;
         }
     } else if (playerSelection == "paper") { 
         if ( computerSelection == "rock") { 
             message.textContent = "You Win! Paper beats rock.";
-            return 1;
+            playerScore.textContent = playerScoreVal + 1;
         } else { 
             message.textContent = "You Lose! Scissors beats paper.";
-            return 0;
+            computerScore.textContent = computerScoreVal + 1;
         }
     } else if (playerSelection == "scissors") { 
         if ( computerSelection == "paper") { 
             message.textContent = "You Win! Scissors beats paper.";
-            return 1;
+            playerScore.textContent = playerScoreVal + 1;
         } else { 
             message.textContent = "You Lose! Rock beats scissors.";
-            return 0; 
+            computerScore.textContent = computerScoreVal + 1; 
+        }
+    }
+
+    if (playerScore.textContent == "5" || computerScore.textContent == "5") {
+        const buttonsDiv = document.querySelector(".buttons"); 
+        const buttons = buttonsDiv.childNodes; 
+        buttons.forEach((button) => { 
+            button.removeEventListener("click", playRound); 
+        });
+
+        console.log("done!"); 
+        if (playerScoreVal > computerScoreVal) { 
+            message.textContent = "YOU WON THE GAME!";  
+
+        } else { 
+            message.textContent = "YOU LOST THE GAME."
         }
     }
 }
 
-const buttons = document.querySelectorAll("button"); 
+function game() {
+    const buttons = document.querySelectorAll("button"); 
 
-buttons.forEach((button) => { 
-    button.addEventListener("click", playRound); 
-})
+    buttons.forEach((button) => { 
+        button.addEventListener("click", playRound); 
+    }); 
+    
+    
+}
+
+game(); 
