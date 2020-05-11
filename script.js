9e0e850822e8d64ef2533fbf00054a94e433a4f1
Nissa <nmnissa@gmail.com>
2020-05-11 13:44:34 -0700

    Changed event handler to play a round

--- script.js
+++ script.js
@@ -4,20 +4,42 @@ function computerPlay() {
     return MOVES[Math.floor(Math.random() * 3)];
 }
 
-function playerPlay() {  
-    const playerChoice = this.id; 
-    if (playerChoice == "rock") { 
-        console.log("rock"); 
-    } else if (playerChoice == "paper") { 
-        console.log("paper"); 
-    } else if (playerChoice == "scissors") { 
-        console.log("scissors"); 
+function playRound() { 
+    const playerSelection = this.id; 
+    const computerSelection = computerPlay(); 
+    const message = document.querySelector("#message"); 
+ 
+    if (playerSelection == computerSelection) { 
+        message.textContent = "Tie!"; 
+    } else if (playerSelection == "rock") { 
+        if ( computerSelection == "scissors") { 
+            message.textContent = "You Win! Rock beats scissors.";
+            return 1;
+        } else { 
+            message.textContent = "You Lose! Paper beats rock.";
+            return 0;
+        }
+    } else if (playerSelection == "paper") { 
+        if ( computerSelection == "rock") { 
+            message.textContent = "You Win! Paper beats rock.";
+            return 1;
+        } else { 
+            message.textContent = "You Lose! Scissors beats paper.";
+            return 0;
+        }
+    } else if (playerSelection == "scissors") { 
+        if ( computerSelection == "paper") { 
+            message.textContent = "You Win! Scissors beats paper.";
+            return 1;
+        } else { 
+            message.textContent = "You Lose! Rock beats scissors.";
+            return 0; 
+        }
     }
 }
 
-
-const buttons = document.querySelectorAll('button'); 
+const buttons = document.querySelectorAll("button"); 
 
 buttons.forEach((button) => { 
-    button.addEventListener('click', playerPlay); 
+    button.addEventListener("click", playRound); 
 })
